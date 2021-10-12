048a98ced8aed0b2f06ec24029f18880516aea67
Willsem <happymainer@gmail.com>
2021-10-12 20:50:42 +0300

    add routes to messages handler

--- hw04/internal/handlers/messages.go
+++ hw04/internal/handlers/messages.go
@@ -1,6 +1,9 @@
 package handlers
 
-import "github.com/sirupsen/logrus"
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/sirupsen/logrus"
+)
 
 type MessagesHandler struct {
 	messagesService MessagesService
@@ -12,4 +15,10 @@ func NewMessagesHandler(messagesService MessagesService, logger *logrus.Logger) 
 		messagesService: messagesService,
 		logger:          logger,
 	}
+}
+
+func (handler *MessagesHandler) Routes() chi.Router {
+	r := chi.NewRouter()
+
+	return r
 }
