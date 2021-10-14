fa0fd0e94ba6b9601705c0ef61e50420a9dfeb58
Willsem <happymainer@gmail.com>
2021-10-15 00:59:42 +0300

    make repositories

--- hw04/internal/repositories/messages.go
+++ hw04/internal/repositories/messages.go
@@ -5,28 +5,48 @@ import (
 	"github.com/willsem/tfs-go-hw/hw04/internal/domain"
 )
 
+const (
+	sharedChatId = "shared"
+)
+
 type MessagesRepository struct {
-	logger *logrus.Logger
+	logger   *logrus.Logger
+	messages []domain.Message
 }
 
 func NewMessagesRepository(logger *logrus.Logger) *MessagesRepository {
 	return &MessagesRepository{
-		logger: logger,
+		logger:   logger,
+		messages: make([]domain.Message, 0),
 	}
 }
 
 func (repository *MessagesRepository) GetSharedMessages() ([]domain.Message, error) {
-	return nil, nil
+	result := make([]domain.Message, 0)
+	for _, message := range repository.messages {
+		if message.RecipentID == sharedChatId {
+			result = append(result, message)
+		}
+	}
+	return result, nil
 }
 
 func (repository *MessagesRepository) GetMessages(user1, user2 string) ([]domain.Message, error) {
-	return nil, nil
+	result := make([]domain.Message, 0)
+	for _, message := range repository.messages {
+		if message.SenderID == user1 && message.RecipentID == user2 ||
+			message.SenderID == user2 && message.RecipentID == user1 {
+			result = append(result, message)
+		}
+	}
+	return result, nil
 }
 
 func (repository *MessagesRepository) AddMessage(message domain.Message) error {
+	repository.messages = append(repository.messages, message)
 	return nil
 }
 
 func (repository *MessagesRepository) GetSharedChatId() string {
-	return ""
+	return sharedChatId
 }
--- hw04/internal/repositories/users.go
+++ hw04/internal/repositories/users.go
@@ -1,40 +1,92 @@
 package repositories
 
 import (
+	"fmt"
+
+	"github.com/google/uuid"
 	"github.com/sirupsen/logrus"
 	"github.com/willsem/tfs-go-hw/hw04/internal/domain"
 )
 
 type UsersRepository struct {
-	logger *logrus.Logger
+	logger   *logrus.Logger
+	users    []domain.User
+	sessions []domain.Session
 }
 
 func NewUsersRepository(logger *logrus.Logger) *UsersRepository {
 	return &UsersRepository{
-		logger: logger,
+		logger:   logger,
+		users:    make([]domain.User, 0),
+		sessions: make([]domain.Session, 0),
 	}
 }
 
 func (repository *UsersRepository) AddUser(login string, passcode string) (domain.User, error) {
-	return domain.User{}, nil
+	user := domain.User{
+		ID:       uuid.New().String(),
+		Login:    login,
+		Passcode: passcode,
+	}
+	repository.users = append(repository.users, user)
+	return user, nil
 }
 
 func (repository *UsersRepository) GetUser(login string) (domain.User, error) {
-	return domain.User{}, nil
+	for _, user := range repository.users {
+		if user.Login == login {
+			return user, nil
+		}
+	}
+	return domain.User{}, fmt.Errorf("No user with that login")
 }
 
 func (repository *UsersRepository) GetUserByID(userId string) (domain.User, error) {
-	return domain.User{}, nil
+	for _, user := range repository.users {
+		if user.ID == userId {
+			return user, nil
+		}
+	}
+	return domain.User{}, fmt.Errorf("No user with that ID")
 }
 
 func (repository *UsersRepository) CreateSession(userId string, token string) error {
+	repository.sessions = append(repository.sessions, domain.Session{
+		UserID: userId,
+		Token:  token,
+	})
 	return nil
 }
 
 func (repository *UsersRepository) RemoveSession(token string) error {
+	removeIndex := -1
+	for i, session := range repository.sessions {
+		if session.Token == token {
+			removeIndex = i
+			break
+		}
+	}
+
+	if removeIndex == -1 {
+		return fmt.Errorf("No session with that token")
+	}
+
+	repository.sessions = append(repository.sessions[:removeIndex], repository.sessions[removeIndex+1:]...)
 	return nil
 }
 
 func (repository *UsersRepository) FindSession(token string) (string, error) {
-	return "", nil
+	index := -1
+	for i, session := range repository.sessions {
+		if session.Token == token {
+			index = i
+			break
+		}
+	}
+
+	if index == -1 {
+		return "", fmt.Errorf("No session with that token")
+	}
+
+	return repository.sessions[index].UserID, nil
 }
