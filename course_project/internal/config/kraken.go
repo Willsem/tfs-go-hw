c339c338f687533a65e6d823116705082415e2f1
Willsem <happymainer@gmail.com>
2021-11-09 13:47:21 +0300

    add secret key to kraken config

--- course_project/internal/config/kraken.go
+++ course_project/internal/config/kraken.go
@@ -2,6 +2,7 @@ package config
 
 type Kraken struct {
 	ApiKey    string `toml:"api_key"`
+	ApiSecret string `toml:"api_secret"`
 	RestUrl   string `toml:"rest_url"`
 	SocketUrl string `toml:"socket_url"`
 }
