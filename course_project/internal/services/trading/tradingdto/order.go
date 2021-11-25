42b48bbac0093e7303dc67c9145db138fe39158e
Willsem <happymainer@gmail.com>
2021-11-25 18:19:39 +0300

    add empty post data for order

--- course_project/internal/services/trading/tradingdto/order.go
+++ course_project/internal/services/trading/tradingdto/order.go
@@ -13,9 +13,14 @@ type Order struct {
 }
 
 func (order Order) GetPostData() string {
+	limitPriceData := ""
+	if order.LimitPrice == 0 {
+		limitPriceData = "limitPrice=" + fmt.Sprintf("%f", order.LimitPrice)
+	}
+
 	return "orderType=" + order.OrderType + "&" +
 		"symbol=" + order.Symbol + "&" +
 		"side=" + order.Side + "&" +
 		"size=" + fmt.Sprintf("%d", order.Size) + "&" +
-		"limitPrice=" + fmt.Sprintf("%f", order.LimitPrice)
+		limitPriceData
 }
